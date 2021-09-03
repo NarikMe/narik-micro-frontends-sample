77094ae119cb075dfeacd0f56a50633199c753e7
ali <abadakhshan@gmail.com>
2021-09-03 13:10:15 +0430

    add  sample-vue-app

---
+++ apps/vue-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <p>
+     <router-link to="/">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+  </p>
+
+   <router-view></router-view>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
